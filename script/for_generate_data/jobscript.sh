5637b7c5b4d395c32c990aba8fb1e1b70e7aa92a
batake-k <takabatake.k.ab@m.titech.ac.jp>
2021-05-03 19:37:20 +0900

    #11 scriptをまとめる

--- script/for_generate_data/jobscript.sh
+++ script/for_generate_data/jobscript.sh
@@ -3,8 +3,6 @@
 #$ -l s_core=1
 #$ -l h_rt=1:00:00
 
-penalty=1
-
 ./hex -i puzzle/M10P1 \
       -o M10P1_B1C1_same_penalty${penalty} \
       -b 1 \
@@ -12,5 +10,5 @@ penalty=1
       -e ${penalty} \
       -s 2 \
       -x true \
-      -y false \
+      -y true \
       -z false
